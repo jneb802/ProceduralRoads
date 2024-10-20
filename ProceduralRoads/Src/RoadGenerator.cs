589854807b1438be6ef9aff8e700c12cfd6d1e52
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-10-19 22:47:56 -0700

    Refactor RoadGenerator to execute per zone

--- ProceduralRoads/Src/RoadGenerator.cs
+++ ProceduralRoads/Src/RoadGenerator.cs
@@ -9,8 +9,7 @@ namespace ProceduralRoads;
 public class RoadGenerator
 {
     public static int roadCount = 100;
-    public static List<RoadGenerator.Road> roads = new List<RoadGenerator.Road>();
-    public static bool roadsRendered = false;
+    public static List<Road> roads = new List<Road>();
     
     public static void PlaceRoads(WorldGenerator worldGenerator)
     {
@@ -30,12 +29,13 @@ public class RoadGenerator
                     p0 = start,
                     p1 = end,
                     center = (start + end) * 0.5f,
-                    widthMax = 5,
-                    widthMin = Random.Range(2,5),
+                    width = Random.Range(2,5),
                     curveWidth = 5
                 };
 
                 roads.Add(road);
+
+                GenerateRoadPoints(road);
             }
         }
 
@@ -48,7 +48,7 @@ public class RoadGenerator
         int attempts = 0;
         do
         {
-            start = new Vector2(UnityEngine.Random.Range(-8000f, 8000f), UnityEngine.Random.Range(-8000f, 8000f));
+            start = new Vector2(UnityEngine.Random.Range(-8000, 8000), UnityEngine.Random.Range(-8000, 8000));
             Heightmap.Biome biome = Heightmap.FindBiome(new Vector3(start.x,0, start.y));
 
             // Check if the point is in the Ocean biome, and retry if it is
@@ -70,7 +70,7 @@ public class RoadGenerator
         int attempts = 0;
         do
         {
-            end = start + new Vector2(UnityEngine.Random.Range(-500f, 500f), UnityEngine.Random.Range(-500f, 500f));
+            end = start + new Vector2(UnityEngine.Random.Range(-500, 500), UnityEngine.Random.Range(-500, 500));
             Heightmap.Biome biome = Heightmap.FindBiome(new Vector3(end.x, 0, end.y));
             
             if (biome != Heightmap.Biome.Ocean)
@@ -85,32 +85,69 @@ public class RoadGenerator
         return false;
     }
 
-    public static void RenderRoads(List<RoadGenerator.Road> roads)
+    public static void GenerateRoadPoints(Road road)
     {
-        foreach (var road in roads)
-        {
-            Debug.Log("Rendering road at point " + road.p0);
-            
-            float segmentLength = road.widthMin * 2;
-            Vector2 direction = (road.p1 - road.p0).normalized;
-            Vector2 perpendicular = new Vector2(-direction.y, direction.x);
-            float distance = Vector2.Distance(road.p0, road.p1);
+        Debug.Log("Generating road points for road at point " + road.p0);
         
-            for (float i = 0; i <= distance; i += segmentLength)
+        List<RoadPoint> roadPoints = new List<RoadPoint>();
+        
+        float segmentLength = 5;
+        Vector2 direction = (road.p1 - road.p0).normalized;
+        Vector2 perpendicular = new Vector2(-direction.y, direction.x); 
+        float distance = Vector2.Distance(road.p0, road.p1);
+        
+        float noiseScale = 0.05f;
+        float noiseOffset = UnityEngine.Random.Range(0f, 1000f);
+
+        for (float i = 0; i <= distance; i += segmentLength)
+        {
+            Vector2 roadPoint = road.p0 + direction * i;
+            
+            float noiseValue = DUtils.PerlinNoise(i * noiseScale + noiseOffset, 0f); 
+            float curveOffset = (noiseValue - 0.5f) * road.curveWidth; 
+            
+            roadPoint += perpendicular * curveOffset;
+            
+            Vector2i roadPointInt = new Vector2i(Mathf.RoundToInt(roadPoint.x), Mathf.RoundToInt(roadPoint.y));
+            Vector3 roadPointVector3 = new Vector3(Mathf.RoundToInt(roadPoint.x), 0,Mathf.RoundToInt(roadPoint.y));
+            
+            Vector2i zoneID = ZoneSystem.instance.GetZone(roadPointVector3);
+            
+            roadPoints.Add(new RoadPoint
             {
-                Vector2 roadPoint = road.p0 + direction * i + perpendicular * 0;
-                
-                ModifyTerrainForRoad(roadPoint, road.widthMin);
+                position = roadPointInt,
+                zoneID = zoneID,
+                width = road.width
+            });
+        }
+        
+        road.roadPoints = roadPoints;
+}
+
+    public static List<RoadPoint> GetRoadPointsInZone(Vector2i zoneID)
+    {
+        List<RoadPoint> roadPointsInZone = new List<RoadPoint>();
+        
+        foreach (Road road in roads)
+        {
+            foreach (RoadPoint roadPoint in road.roadPoints)
+            {
+                if (roadPoint.zoneID == zoneID)
+                {
+                    roadPointsInZone.Add(roadPoint); 
+                }
             }
         }
+        
+        return roadPointsInZone;
     }
     
-    private static void ModifyTerrainForRoad(Vector2 roadPoint, float width)
+    public static void RenderRoadPoint(RoadPoint roadPoint)
     {
-        Debug.Log("Modifying terrain at point " + roadPoint);
+        Debug.Log("Modifying terrain at point " + roadPoint.position);
         
-        Vector3 roadPositionTemp = new Vector3(roadPoint.x, 0, roadPoint.y);
-        Vector3 roadPosition = new Vector3(roadPoint.x, ZoneSystem.instance.GetGroundHeight(roadPositionTemp), roadPoint.y);
+        Vector3 roadPositionTemp = new Vector3(roadPoint.position.x, 0, roadPoint.position.y);
+        Vector3 roadPosition = new Vector3(roadPoint.position.x, ZoneSystem.instance.GetGroundHeight(roadPositionTemp), roadPoint.position.y);
 
         // Get the heightmap for the given road point
         Heightmap heightmap = Heightmap.FindHeightmap(roadPosition);
@@ -119,36 +156,61 @@ public class RoadGenerator
             Debug.Log("Heightmap at position " + roadPosition + " is null");
             return;
         }
-
-        // Get terrain compiler to modify terrain
-        TerrainComp terrainComp = heightmap.GetAndCreateTerrainCompiler();
-        if (terrainComp == null || !terrainComp.IsOwner()) return;
+        
+        // Trying out another approach below
+        // TerrainComp terrainComp = heightmap.GetAndCreateTerrainCompiler();
+        
+        TerrainComp terrainComp = heightmap.m_terrainCompilerPrefab.GetComponent<TerrainComp>();
+        if (terrainComp == null)
+        {
+            Debug.Log("Terrain compiler is null");
+            return;
+            // || !terrainComp.IsOwner()
+        }
 
         // Create a terrain operation for flattening and paving the road
+        TerrainOp modifier = new TerrainOp();
+        modifier.transform.position = roadPosition;
         TerrainOp.Settings modifierSettings = new TerrainOp.Settings
         {
             m_level = true,
-            m_levelRadius = width,
+            m_levelRadius = roadPoint.width,
             m_smooth = true,
-            m_smoothRadius = width + 3f,
-            m_smoothPower = Random.Range(4,6),
+            m_smoothRadius = roadPoint.width + 6f,
+            m_smoothPower = 10,
             m_paintCleared = true,
             m_paintType = TerrainModifier.PaintType.Paved,
-            m_paintRadius = width,
-            m_levelOffset = new Vector3(roadPoint.x, ZoneSystem.instance.GetGroundHeight(roadPositionTemp), roadPoint.y).y
+            m_paintRadius = roadPoint.width,
+            m_square = false
+            // m_levelOffset = new Vector3(roadPoint.position.x, ZoneSystem.instance.GetGroundHeight(roadPositionTemp), roadPoint.position.y).y
         };
-        
-        terrainComp.DoOperation(roadPosition, modifierSettings);
+        modifier.m_settings = modifierSettings;
+
+        if (heightmap != null)
+        {
+            // terrainComp.ApplyOperation(modifier);
+            
+            // Trying out another approach above
+            terrainComp.DoOperation(roadPosition, modifierSettings);
+        }
     }
     
     public class Road
     {
-        public Vector2i p0;
-        public Vector2i p1;
-        public Vector2i center;
-        public float widthMin;
-        public float widthMax;
+        public Vector2 p0;
+        public Vector2 p1;
+        public Vector2 center;
+        public float width;
         public float curveWidth;
+        public List<RoadPoint> roadPoints;
+    }
+
+    public class RoadPoint
+    {
+        public int roadPointIndex;
+        public Vector2i position;
+        public Vector2i zoneID;
+        public float width;
     }
     
 }

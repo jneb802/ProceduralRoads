8f608ffeb9574450c7912eee1f7357a953de687b
jneb802 <73610029+jneb802@users.noreply.github.com>
2026-01-19 23:27:21 -0800

    version increment

--- ProceduralRoads/Src/Plugin.cs
+++ ProceduralRoads/Src/Plugin.cs
@@ -15,7 +15,7 @@ namespace ProceduralRoads
     public class ProceduralRoadsPlugin : BaseUnityPlugin
     {
         internal const string ModName = "ProceduralRoads";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.1.0";
         internal const string Author = "warpalicious";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
